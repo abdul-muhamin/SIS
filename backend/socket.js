587c0ec71434368d8007bc935f8dc66e09293569
RohmaButt <rohma.butt@gmail.com>
2024-11-12 07:37:20 +0500

    socket fixes

--- backend/socket.js
+++ backend/socket.js
@@ -4,7 +4,7 @@ const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
       origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
+      // methods: ["GET", "POST"],
     },
   });
 
@@ -13,9 +13,8 @@ const setupSocket = (server) => {
 
     // Join room based on user ID from the client side
     // socket.on("joinRoom", (userId) => {
-    //     socket.join(userId);
-    //     console.log(`User ${userId} joined room ${userId}`);
-      
+    //   socket.join(userId);
+    //   console.log(`User ${userId} joined room ${userId}`);
     // });
 
     // socket.on("sendNotification", (data) => {
@@ -23,8 +22,20 @@ const setupSocket = (server) => {
     //   // Emit only to the room associated with toUserId
     //   io.to(data.toUserId).emit("receiveNotification", data);
     // });
-    socket.on("read_msg", (arg) => {      console.log(arg);  'world from client'    });
-    socket.emit('send_msg',  'emit from server');
+
+    ////2-way immediate msgs --> From Server to Client usecase
+    // socket.on("chat_message", (arg) => {
+    //   console.log("msg at server:", arg);
+    //   io.emit("following_chat_message", "response from server");
+    // });
+
+    ////From Server to Client usecase
+    // socket.emit("server-chat_message", "this is a msg from server");
+
+    ////From Client to Server usecase
+    socket.on("client-chat_message", (msg) => {
+      console.log("client msg at server:", msg);
+    });
 
     socket.on("disconnect", () => {
       console.log("A user disconnected:", socket.id);
--- frontend/src/layouts/dashboard/index.jsx
+++ frontend/src/layouts/dashboard/index.jsx
@@ -1,5 +1,5 @@
-import io from 'socket.io-client';
 import PropTypes from 'prop-types';
+import { io } from 'socket.io-client';
 import { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
@@ -8,12 +8,50 @@ import Nav from './nav';
 import Main from './main';
 import Header from './header';
 
-
 export default function DashboardLayout({ children }) {
   const [openNav, setOpenNav] = useState(false);
- 
 
+  useEffect(() => {
+    console.log('test1');
 
+    function onConnect() {
+      console.log('onConnect');
+    }
+
+    function onDisconnect() {
+      console.log('onDisconnect');
+    }
+
+    function onFooEvent(value) {
+      console.log('2');
+    }
+
+    const socket = io('http://localhost:3001');
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    ////2-way immediate msgs --> From Server to Client usecase
+    // socket.emit('chat_message', 'msg 1 from client');
+    // socket.on('following_chat_message', (arg) => {
+    //   console.log('msg 2 at client:', arg);
+    // });
+
+    ////From Server to Client usecase
+    // socket.on('server-chat_message', (msg) => {
+    //   console.log('msg at client:', msg);
+    // });
+
+    ////From Client to Server usecase
+    socket.emit('client-chat_message', 'this is a msg to server');
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('chat_message', onFooEvent);
+      socket.off('following_chat_message', onFooEvent);
+      socket.off('client-chat_message', onFooEvent);
+    };
+  }, []);
 
   return (
     <>
